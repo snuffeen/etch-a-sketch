cd48a822cca9b51d6c10da32adc6b2d4ca1427a5
snuffeen <133567875+snuffeen@users.noreply.github.com>
2024-06-10 16:35:28 +0300

    added a button that takes user input inputs are saved as horizontal and vertical squares inputs are then multiplied to get a total amount

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+let inputHorizontalSquares = 0
+let inputVerticalSquares = 0
+let totalSquares = 0
+
+const button = document.querySelector("button")
+button.addEventListener("click", createGrid)
 
 
-const totalSquares = horizontalSquares*verticalSquares
+function getUserInput() {
+    inputHorizontalSquares = prompt("horizontal")
+    inputVerticalSquares = prompt("vertical")
+}
 
-function createGrid (totalSquares){
+function countSquares() {
+    getUserInput()
+    totalSquares = inputHorizontalSquares*inputVerticalSquares
+    return(totalSquares)
+}
 
+function createGrid() {
+    countSquares()
+    
 }
