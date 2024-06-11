9839efe8922f9bd2f540b4d803988cd7abaec16f
snuffeen <133567875+snuffeen@users.noreply.github.com>
2024-06-11 18:27:12 +0300

    reworked createGrid function now creates x number of horizontal divs and y number of vertical divs inside of them rather than x*y number of divs

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let inputHorizontalSquares = 0
 let inputVerticalSquares = 0
-let totalSquares = 0
 
 const button = document.querySelector("button")
 button.addEventListener("click", createGrid)
@@ -23,22 +22,21 @@ function getUserInput() {
     }
 }
 
-function countSquares() {
-    getUserInput()
-    totalSquares = inputHorizontalSquares*inputVerticalSquares
-    return(totalSquares)
-}
-
 function createGrid() {
-    countSquares()
-    
+    getUserInput()
+
     while (container.firstChild) {
         container.removeChild(container.firstChild)
     }
     
-    for (let i = 0; i < totalSquares; i++) {
-        const content = document.createElement("div")
-        content.classList.add("content")
-        container.appendChild(content)
+    for (let i = 0; i < inputHorizontalSquares; i++) {
+        const horizontal = document.createElement("div")
+        horizontal.classList.add("horizontal")
+        container.appendChild(horizontal)
+        for (let j = 0; j < inputVerticalSquares; j++) {
+            const vertical = document.createElement("div")
+            vertical.classList.add("vertical")
+            horizontal.appendChild(vertical)
+        }
     }
 }
